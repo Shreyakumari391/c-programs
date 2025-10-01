acdd9396f3c4149dd4d884e051bceba4ec7cfbd4
Shreyakumari391 <shreyasinha391@gmail.com>
2025-10-01 19:54:02 +0530

    initial commit

---
+++ addition.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main () {
+    int num1, num2, sum;
+
+    // Ask the user to input two numbers
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Add the two numbers
+    sum = num1 + num2;
+
+    // Display the result
+    printf("Sum: %d\n", sum);
+
+    return 0;
+
+}
